09b24e580c753966138c58f6bd1e8ac728ede59c
煜豪 林 <1156641208@qq.com>
2022-03-16 22:18:26 +0800

    1

--- 1 Two Sum/twoSum2.cpp
+++ 1 Two Sum/twoSum2.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <vector>
+#include <unordered_map>
+#include <utility>
 
 using namespace std;
 
@@ -8,5 +10,20 @@ class Solution
 public:
     vector<int> twoSum(vector<int> &nums, int target)
     {
+        unordered_map<int, int> result_map;
+        vector<int> result;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (result_map.find(nums[i]) != result_map.end())
+            {
+                result = {result_map.find(nums[i])->second, i};
+                return result;
+            }
+            else
+            {
+                result_map.insert(pair<int, int>(target - nums[i], i));   
+            }
+        }
+        return result;
     }
 };
