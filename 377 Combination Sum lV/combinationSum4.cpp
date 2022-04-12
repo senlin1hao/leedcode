6facd4a33bef1fa4148e234ce98910e0a688f98b
sen lin1hao <1156641208@qq.com>
2022-04-12 10:31:14 +0800

    377

--- 377 Combination Sum lV/combinationSum4.cpp
+++ 377 Combination Sum lV/combinationSum4.cpp
@@ -8,7 +8,7 @@ class Solution
 public:
     int combinationSum4(vector<int> &nums, int target)
     {
-        //中间结果存在超出int范围dp[i]，使用unsigned int
+        //结果不超过int，但中间结果存在超出int范围的dp[i]，使用unsigned int
         vector<unsigned int> dp(target + 1, 0);
         dp[0] = 1;
 
