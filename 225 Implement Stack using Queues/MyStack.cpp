9e5e0cb90bb02e4e4c8acb4348aaf3ca4ee4c424
煜豪 林 <1156641208@qq.com>
2022-04-18 09:26:13 +0800

    225

---
+++ 225 Implement Stack using Queues/MyStack.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+class MyStack
+{
+private:
+    queue<int> queue1;
+
+public:
+    MyStack()
+    {
+    }
+
+    void push(int x)
+    {
+        queue1.push(x);
+    }
+
+    int pop()
+    {
+        for (int i = 0; i < queue1.size() - 1; i++)
+        {
+            queue1.push(queue1.front());
+            queue1.pop();
+        }
+        int result = queue1.front();
+        queue1.pop();
+        return result;
+    }
+
+    int top()
+    {
+        for (int i = 0; i < queue1.size() - 1; i++)
+        {
+            queue1.push(queue1.front());
+            queue1.pop();
+        }
+        int result = queue1.front();
+        queue1.push(queue1.front());
+        queue1.pop();
+        return result;
+    }
+
+    bool empty()
+    {
+        return queue1.empty();
+    }
+};
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack* obj = new MyStack();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->top();
+ * bool param_4 = obj->empty();
+ */
