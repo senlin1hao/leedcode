55e11a23fbb360e43d1a98ba723881e062ac4583
sen lin1hao <1156641208@qq.com>
2022-04-19 10:10:47 +0800

    347

---
+++ 347 Top K Frequent Elements/topKFrequent1.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <algorithm>
+
+using namespace std;
+
+//哈希法+vector+排序
+class Solution
+{
+private:
+    //注意这里需要static
+    static bool compare(pair<int, int> p1, pair<int, int> p2)
+    {
+        return (p1.second > p2.second);
+    }
+
+public:
+    vector<int> topKFrequent(vector<int> &nums, int k)
+    {
+        map<int, int> numsMap;
+        for (int i : nums)
+        {
+            ++numsMap[i];
+        }
+
+        vector<pair<int, int>> v(numsMap.begin(), numsMap.end());
+        sort(v.begin(), v.end(), compare);
+
+        vector<int> result;
+        for (int i = 0; i < k; i++)
+        {
+            result.push_back(v[i].first);
+        }
+        
+        return result;
+    }
+};
