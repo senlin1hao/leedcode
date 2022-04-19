4a11b268fae54a1ffce801c57245b9e3a6c6e856
sen lin1hao <1156641208@qq.com>
2022-04-19 09:29:10 +0800

    239

--- 239 Sliding Window Maximum/maxSlidingWindow.cpp
+++ 239 Sliding Window Maximum/maxSlidingWindow.cpp
@@ -1,12 +1,65 @@
 #include <iostream>
 #include <vector>
+#include <deque>
 
 using namespace std;
+
+//单调队列
+class MonotonousQueue
+{
+private:
+    deque<int> numsDeque;
+
+public:
+    MonotonousQueue(){};
+    ~MonotonousQueue(){};
+
+    void push(int x)
+    {
+        while ((!numsDeque.empty()) && (x > numsDeque.back()))
+        {
+            numsDeque.pop_back();
+        }
+
+        numsDeque.push_back(x);
+    }
+
+    void pop()
+    {
+        numsDeque.pop_front();
+    }
+
+    int front()
+    {
+        return numsDeque.front();
+    }
+};
 
 class Solution
 {
 public:
     vector<int> maxSlidingWindow(vector<int> &nums, int k)
     {
+        MonotonousQueue numsQueue;
+        vector<int> result;
+
+        for (int i = 0; i < k; i++)
+        {
+            numsQueue.push(nums[i]);
+        }
+        result.push_back(numsQueue.front());
+
+        for (int i = k; i < nums.size(); i++)
+        {
+            if (nums[i - k] == numsQueue.front())
+            {
+                numsQueue.pop();
+            }
+
+            numsQueue.push(nums[i]);
+            result.push_back(numsQueue.front());
+        }
+
+        return result;
     }
 };
