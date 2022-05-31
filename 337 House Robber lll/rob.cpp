c936dda4fc8a52e21618f22649a0eec2e3e443fc
sen lin1hao <1156641208@qq.com>
2022-05-31 14:53:48 +0800

    337

--- 337 House Robber lll/rob.cpp
+++ 337 House Robber lll/rob.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <math.h>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
@@ -15,8 +17,30 @@ struct TreeNode
 
 class Solution
 {
+private:
+    vector<int> robTree(TreeNode *parent)
+    {
+        if (parent == nullptr)
+        {
+            return {0, 0};
+        }
+
+        vector<int> left = robTree(parent->left);
+        vector<int> right = robTree(parent->right);
+
+        //抢劫parent节点
+        int result1 = parent->val + left[1] + right[1];
+
+        //不抢劫parent节点
+        int result2 = max(left[0], left[1]) + max(right[0], right[1]);
+
+        return {result1, result2};
+    }
+
 public:
     int rob(TreeNode *root)
     {
+        vector<int> result = robTree(root);
+        return max(result[0], result[1]);
     }
 };
