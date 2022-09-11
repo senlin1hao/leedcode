09db7dc021b41702407f35607015d695a7fbff41
sen lin1hao <1156641208@qq.com>
2022-09-11 21:43:57 +0800

    123

--- 123 Best Time to Buy and Sell Stock III/maxProfit3.cpp
+++ 123 Best Time to Buy and Sell Stock III/maxProfit3.cpp
@@ -10,8 +10,7 @@ class Solution
 public:
     int maxProfit(vector<int> &prices)
     {
-        vector<vector<int>> dp(prices.size(), vector<int>(5, 0));
-        dp[0] = dp[2] = dp[4] = 0;
-        dp[1] = dp[3];
+        vector<int> dp(5, 0);
+        dp[1] = dp[3] = -prices[0];
     }
 };
